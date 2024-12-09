bca8f7ecdd276e701c6580561c90a11bd97c0ee9
Roman Artemyev <artemyev.ram@gmail.com>
2024-12-09 14:12:17 +0300

    Blade layouts

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'index')->name('index');
-Route::get('/courses', fn() => 'Courses')->name('сourses');
-//Route::get('/', 'Contact')->name('contact');
-//Route::get('/', 'Login')->name('login');
-//Route::get('/', 'Register')->name('register');
+Route::view('/courses', 'courses')->name('сourses');
+Route::view('/contact', 'contact')->name('contact');
+Route::view('/login', 'login')->name('login');
+Route::view('/register', 'register')->name('register');
