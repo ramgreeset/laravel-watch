145e29cae488e809feccc73cdac343ddf0dd4cd8
Roman Artemyev <artemyev.ram@gmail.com>
2024-12-23 20:54:46 +0300

    Мелкие правки в коде. Отвалилась иконка ютуб.

--- resources/views/components/courses.blade.php
+++ resources/views/components/courses.blade.php
@@ -1,6 +1,6 @@
 <section>
     <div class="container px-4 py-2">
-        <h2 class="text-4xl text-gray-400 mb-4">Latest Courses</h2>
+        <h2 class="text-5xl text-gray-400 text-center mb-20">Latest Courses</h2>
         <div class="flex justify-between gap-2">
 
             @foreach(range(0, 3) as $i)
@@ -17,17 +17,11 @@
                         <footer class="flex flex-col items-end font-semibold">
                             <div>
                                 <div class="flex items-center gap-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="size-4" viewBox="0 0 512 512">
-                                        <path
-                                            d="M0 96C0 60.7 28.7 32 64 32l384 0c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L64 480c-35.3 0-64-28.7-64-64L0 96zM48 368l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16zm368-16c-8.8 0-16 7.2-16 16l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0zM48 240l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16zm368-16c-8.8 0-16 7.2-16 16l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0zM48 112l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16L64 96c-8.8 0-16 7.2-16 16zM416 96c-8.8 0-16 7.2-16 16l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0zM160 128l0 64c0 17.7 14.3 32 32 32l128 0c17.7 0 32-14.3 32-32l0-64c0-17.7-14.3-32-32-32L192 96c-17.7 0-32 14.3-32 32zm32 160c-17.7 0-32 14.3-32 32l0 64c0 17.7 14.3 32 32 32l128 0c17.7 0 32-14.3 32-32l0-64c0-17.7-14.3-32-32-32l-128 0z"/>
-                                    </svg>
+                                    {!! config('watch.course_icons.film') !!}
                                     <span>23 lessons</span>
                                 </div>
                                 <div class="flex items-center gap-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="size-4" viewBox="0 0 512 512">
-                                        <path
-                                            d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120l0 136c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2 280 120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/>
-                                    </svg>
+                                    {!! config('watch.course_icons.clock') !!}
                                     <span>2h 30m</span>
                                 </div>
                             </div>
--- resources/views/components/footer/index.blade.php
+++ resources/views/components/footer/index.blade.php
@@ -1,9 +1,3 @@
-@php
-
-
-
-    @endphp
-
 <footer class="bg-violet-600">
     <div class="flex flex-col gap-8 container py-4 px-4">
         <div class="flex items-center gap-8 justify-between">
